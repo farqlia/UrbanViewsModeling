98c63c1337f485023f90241d16af17dd2c1568aa
julia <juliafarganus@gmail.com>
2024-10-07 22:59:27 +0200

    Work with point cloud

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -3,15 +3,27 @@ import pyvista as pv
 from pyntcloud import PyntCloud
 
 # Generate random point cloud data
-points = np.random.rand(100, 3)
 
-pt = PyntCloud.from_file('../data/birmingham_block_0/segmented-cloud-subsampled.ply')
+pt = PyntCloud.from_file('../data/birmingham_block_0/segmented-cloud-subsampled-rgb.ply')
 
+coords = pt.points[['x', 'y', 'z']].values
+
+x = (coords[:, 0] - np.mean(coords[:, 0])) / np.std(coords[:, 0])
+y = (coords[:, 1] - np.mean(coords[:, 1])) / np.std(coords[:, 1])
+z = (coords[:, 2] - np.mean(coords[:, 2])) / np.std(coords[:, 2])
 
 # Create a PolyData object from the points
-point_cloud = pv.PolyData(pt.points.loc[:20000, ['x', 'y', 'z']].values)
+point_cloud = pv.PolyData(np.column_stack([x, y, z]))
+# point_cloud = pv.PolyData(coords)
+point_cloud.cell_data['colors'] = pt.points[['red', 'green', 'blue']].values
+
+print(pt.points.columns)
+print(pt.points.iloc[0])
+print(pt.points['red'].max(), pt.points['green'].max(), pt.points['blue'].max())
+print(pt.points['red'].min(), pt.points['green'].min(), pt.points['blue'].min())
 
 # Plot the point cloud
 plotter = pv.Plotter()
-plotter.add_mesh(point_cloud, color="green", point_size=5, render_points_as_spheres=True)
+plotter.add_mesh(point_cloud, point_size=3, render_points_as_spheres=True,
+                 scalars='colors', lighting=False, rgb=True, preference='cell')
 plotter.show()
